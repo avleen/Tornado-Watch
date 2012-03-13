b62ce0d1ccac3b3db91a619bad72edbea90d9cdd
Avleen Vig <avleen@chiku.silverwraith.com>
2012-03-12 23:14:49 -0700

    oops, password got in, revoked it and removed from the file

--- htdocs/register_server_with_c2dm.sh
+++ htdocs/register_server_with_c2dm.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -d 'Email=avleen@gmail.com' -d 'Passwd=exdntceslutqazth' -d 'accountType=GOOGLE' -d 'source=com.silverwraith.tornadowatch' -d 'service=ac2dm' https://www.google.com/accounts/ClientLogin
+curl -d 'Email=avleen@gmail.com' -d 'Passwd=changedit' -d 'accountType=GOOGLE' -d 'source=com.silverwraith.tornadowatch' -d 'service=ac2dm' https://www.google.com/accounts/ClientLogin
