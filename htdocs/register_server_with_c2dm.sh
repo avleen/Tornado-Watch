bff6c0a0b9bf29f4645e2d6be15c3acd137addf9
Avleen Vig <avleen@chiku.silverwraith.com>
2012-03-12 23:18:15 -0700

    remove register_server_with_c2dm.sh

--- htdocs/register_server_with_c2dm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -d 'Email=avleen@gmail.com' -d 'Passwd=changedit' -d 'accountType=GOOGLE' -d 'source=com.silverwraith.tornadowatch' -d 'service=ac2dm' https://www.google.com/accounts/ClientLogin
