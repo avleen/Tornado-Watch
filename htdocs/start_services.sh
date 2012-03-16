d1bfcba3cc77c36d93953a718faffc7af7a33a38
Avleen Vig <avleen@chiku.silverwraith.com>
2012-03-15 23:25:12 -0700

    -amremove unneccesary start, chmod

--- htdocs/start_services.sh
+++ htdocs/start_services.sh
@@ -2,6 +2,5 @@
 
 BASE_DIR="/www/silverwraith.com/canonical/tw.silverwraith.com"
 
-nohup ${BASE_DIR}/pull_weatherfeed.py -d >> ${BASE_DIR}/logs/pull_weatherfeed.log &
 nohup ${BASE_DIR}/add_alerts.py -d >> ${BASE_DIR}/logs/add_alerts.log &
 nohup ${BASE_DIR}/send_alerts.py -d >> ${BASE_DIR}/logs/send_alerts.log &
