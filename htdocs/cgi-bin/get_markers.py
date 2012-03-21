94dd9801fb29a3f2b69f89b3c02913b4c114eb2a
Avleen Vig <avleen@gmail.com>
2012-03-21 00:45:21 -0700

    only show markers when more than one person with 20 miles submits a marker

--- htdocs/cgi-bin/get_markers.py
+++ htdocs/cgi-bin/get_markers.py
@@ -19,11 +19,32 @@ def make_db_conn():
 def main():
     make_db_conn()
     dict_cur = DB_CONN.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    sql = """SELECT X(location) AS lng, Y(location) AS lat, priority
-                FROM user_submits
-                WHERE create_date > (%s - (60 * 60 * 24))"""
-    dict_cur.execute(sql, (TIME,))
-    jsonout = json.dumps(dict_cur.fetchall())
+    marker_list = []
+
+    all_markers_sql = """SELECT id, location, X(location) AS lng, Y(location) AS lat, priority
+                            FROM user_submits
+                            WHERE create_date > (%s - (60 * 60 * 24))"""
+    dict_cur.execute(all_markers_sql, (TIME,))
+
+    # For each marker submitted, do a distance check. If there is ONE other
+    # marker within 10 miles, this is possibly legit and we'll display it.
+    for row in dict_cur:
+        check_sql = """SELECT SUM(weight)
+                        FROM user_submits
+                        WHERE create_date > (%s - (60 * 60 * 24))
+                        AND ST_DWithin(%s, location, 32186, false)"""
+        check_cur = DB_CONN.cursor()
+        check_cur.execute(check_sql, (TIME, row['location']))
+        check_row = check_cur.fetchone()
+        if check_row[0] >= 4:
+            marker_list.append({'lat': row['lat'],
+                                'lng': row['lng'],
+                                'priority': row['priority']})
+        check_cur.close()
+    if len(marker_list) == 0:
+        marker_list = [{"lat": 0, "lng": 90, "priority": 'f'}]
+    jsonout = json.dumps(marker_list)
+    print "markers: %d" % len(marker_list)
 
     print "Content-type: text/plain"
     print
