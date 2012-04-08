3890349dc24ed486aadd8652952cf844d2b225b6
Avleen Vig <avleen@gmail.com>
2012-04-07 20:30:57 -0700

    minute admindash updates

--- htdocs/htdocs/admindash.php
+++ htdocs/htdocs/admindash.php
@@ -12,8 +12,11 @@ $conn = pg_connect("dbname=tornadowatch user=postgres");
         $count = pg_fetch_row($result);
         echo $count[0]; ?>
         <br>
-        <a href="/cgi-bin/make_kml.sh" target="_blank">Current users map</a>
+        <a href="/cgi-bin/make_kml.sh" target="_blank">Current users map</a><br>
         <a href="/cgi-bin/make_kml_circle.sh" target="_blank">Current users map circles</a>
         <br>
+        <?php exec("/www/silverwraith.com/canonical/tw.silverwraith.com/cgi-bin/make_registrations_graph.sh"); ?>
+        <img src="/user_registrations_daily.png"></img><br>
+        <img src="/user_registrations_hourly.png"></img><br>
     </body>
 </html>
