2dc5106bab2e49e1ae8f85ffda76e8f69582e813
Avleen Vig <avleen@gmail.com>
2012-04-07 20:32:33 -0700

    add log files to get output in start_services.sh

--- htdocs/start_services.sh
+++ htdocs/start_services.sh
@@ -2,5 +2,5 @@
 
 BASE_DIR="/www/silverwraith.com/canonical/tw.silverwraith.com"
 
-nohup ${BASE_DIR}/add_alerts.py -d 2>&1 &
-nohup ${BASE_DIR}/send_alerts.py -d 2>&1 &
+nohup ${BASE_DIR}/add_alerts.py -d >> ${BASE_DIR}/logs/add_alerts.out 2>&1 &
+nohup ${BASE_DIR}/send_alerts.py -d >> ${BASE_DIR}/logs/send_alerts.out 2>&1 &
